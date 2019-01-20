60bad99c507bb06dc74c87bb373954b1f119037f
Jan-Nilas Gerth <janx1644@edu.eal.dk>
2019-01-20 14:06:20 +0100

    Styling of the list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,10 +7,18 @@
      <div class="to-do-card"> 
         <input class="to-do-card--input" placeholder="Add new items.."/>
         <div class="to-do-card--item"> 
-            <h2 class="main-heading-small to-do-card-heading"> Mandag</h2>
-            <img src="" class="to-do-card--img"/>
+            <h2 class="main-heading-small to-do-card-heading">Mandag</h2>
+           <!-- <script type="text/x-template" id="circle"> 
+           <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs>
+               <style>.cls-1,.cls-2{fill:none;}.cls-1{stroke:#1d1d1b;stroke-miterlimit:10;stroke-width:2px;}</style>
+               </defs><title>button-circle2</title>
+               <g id="Ellipse_2" data-name="Ellipse 2">
+                   <circle class="cls-1" cx="12" cy="12" r="11"/>
+                   <circle class="cls-2" cx="12" cy="12" r="10"/></g>
+                   </svg>
+                   </script>     -->
         </div>
      </div>
-     <button class="main-button--mainc" />
+     <button class="main-button--mainc">Edit</button>
   </div>
 </template>
